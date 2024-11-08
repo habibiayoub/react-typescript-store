1a190fb7070937859292f0302109fe5dcbd73076
Habibi ayoub <habibiayoub87@gmail.com>
2024-11-08 17:17:19 +0100

    Store app with react and typescript

---
+++ src/components/Filters.tsx
@@ -0,0 +1,37 @@
+import { Form, useLoaderData, Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import { ProductsResponseWithParams } from 'src/utils';
+import { FormInput } from '../components';
+import { FormSelect } from '../components';
+import { FormRange } from '../components';
+import { FormCheckbox } from '../components';
+
+function Filters() {
+
+    const { meta, params } = useLoaderData() as ProductsResponseWithParams;
+    const { search, company, category, shipping, order, price } = params;
+
+    return (
+        <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
+            <FormInput type='search' label='search product' name='search' defaultValue={search} />
+            <FormSelect label='select category' name='category' options={meta.categories} defaultValue={category} />
+            <FormSelect label='select company' name='company' options={meta.companies} defaultValue={company} />
+            <FormSelect label='order by' name='order' options={['a-z', 'z-a', 'high', 'low']} defaultValue={order} />
+            <FormRange label='price' name='price' defaultValue={price} />
+            <FormCheckbox label='free shipping' name='shipping' defaultValue={shipping} />
+            <Button type='submit' size='sm' className='self-end mb-2'>
+                search
+            </Button>
+            <Button
+                type='button'
+                asChild
+                size='sm'
+                variant='outline'
+                className='self-end mb-2'
+            >
+                <Link to='/products'>reset</Link>
+            </Button>
+        </Form>
+    );
+}
+export default Filters;
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,19 @@
+import { type LoaderFunction } from "react-router-dom";
+import { Hero, FeaturedProducts } from "../components";
+import { customFetch, type ProductsResponse } from "../utils";
+
+const url = '/products?featured=true';
+
+export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
+    const response = await customFetch<ProductsResponse>(url);
+    return { ...response.data };
+};
+
+function Landing() {
+
+    return <>
+        <Hero />
+        <FeaturedProducts />
+    </>
+}
+export default Landing;
